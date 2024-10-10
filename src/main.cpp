08cc6cb2926f247d22f2e1591d1957da0a5c6130
Marek Cerny <me@marekcerny.com>
2024-10-10 15:36:26 +0200

    add threads

--- src/main.cpp
+++ src/main.cpp
@@ -82,9 +82,9 @@ auto compute_equivalence(std::string name, GetGraphs&& get_graphs, int threads, 
 
     #pragma omp parallel for num_threads(threads)
     for (size_t i = 0; i < graph_list.size(); ++i) {
-        colvecs[i].first = get_colors(graph_list[i]);
-        std::sort(colvec.begin(), colvec.end());
-        colvecs[i].second = i;
+        colvecs[i].index = i;
+        colvecs[i].data = get_colors(graph_list[i]);
+        std::sort(colvecs[i].data.begin(), colvecs[i].data.end());
     }
 
     std::cout << "color classes " << unique_size(colvecs) << " (" << name << ") \n";
