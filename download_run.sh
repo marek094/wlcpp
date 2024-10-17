bcf8cb1403761aad753a5960b73fcb5b5053f949
Marek Cerny <me@marekcerny.com>
2024-10-17 11:39:26 +0200

    x

--- download_run.sh
+++ download_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 # Download the data
-git clone -b feature/pathwl https://github.com/marek094/wlcpp.git
+git clone --recursive -b feature/pathwl https://github.com/marek094/wlcpp.git
 
 # Run the program
 
@@ -11,28 +11,29 @@ mkdir build
 
 cd build
 
-sudo apt-get install libboost-all-dev
-sudo apt-get install libopenmpi-dev
-sudo apt-get install cmake curl
+apt install -y libboost-all-dev
+apt install -y libopenmpi-dev
+apt install -y cmake curl
 
 
 for i in {15..10}
 do 
-    sudo apt-get install g++-$i gcc-$i
+    apt-get install -y g++-$i gcc-$i
     if [ $? -eq 0 ]
     then
-        export CXX=g++-$i CC=gcc-$i
+        export cxx=g++-$i cc=gcc-$i
         break
     fi
 done
 
 
-cmake -DCMAKE_BUILD_TYPE=Release ..
+CXX=$cxx CC=$cc cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j 4 main 
 cd ..
 
 
-name=graph9c.g6
+name=${name:="graph9c.g6"}
+cpus=${cpus:=12}
 urladdr=https://users.cecs.anu.edu.au/~bdm/data/$name
 mkdir graph_data
 cd graph_data
@@ -40,4 +41,4 @@ curl -O $urladdr
 cd ..
 
 
-./build/src/main graph_data/$name 12
+./build/src/main graph_data/$name $cpus
